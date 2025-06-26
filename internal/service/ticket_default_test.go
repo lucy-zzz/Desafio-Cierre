1fa663051f7972a6f91030d05710d26dfcedc5ce
Lucyla Loro <lucyla.loro@gmail.com>
2025-06-26 16:48:37 -0300

    test: testing GetTicketsByDestinationCountry

--- internal/service/ticket_default_test.go
+++ internal/service/ticket_default_test.go
@@ -34,3 +34,29 @@ func TestServiceTicketDefault_GetTotalTickets(t *testing.T) {
 		require.Equal(t, expectedTotal, total)
 	})
 }
+
+func TestServiceTicketDefault_GetTicketsByDestinationCountry(t *testing.T) {
+	t.Run("success to get total tickets amount by country", func(t *testing.T) {
+		rp := repository.NewRepositoryTicketMock()
+		rp.FuncGetTicketsByDestinationCountry = func(c string) (ta map[int]internal.TicketAttributes, err error) {
+			ta = map[int]internal.TicketAttributes{
+				1: {
+					Name:    "John",
+					Email:   "johndoe@gmail.com",
+					Country: "USA",
+					Hour:    "10:00",
+					Price:   100,
+				},
+			}
+			return ta, err
+		}
+
+		sv := service.NewServiceTicketDefault(rp)
+
+		total, err := sv.GetTicketsAmountByDestinationCountry("")
+
+		expectedTotal := 1
+		require.NoError(t, err)
+		require.Equal(t, expectedTotal, total)
+	})
+}
